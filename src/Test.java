eb2105c63eb30cad9567501fb755ab5787195a3b
Andreas Schaefer <schaefer.andreas@gmail.com>
2014-10-29 17:31:44 +0100

    A second line

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! 123");
+		System.out.println("Hello World! 123\n");
+		System.out.println("A second line");
 	}
 
 }
